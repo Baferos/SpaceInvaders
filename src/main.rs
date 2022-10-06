61743695dc4d302a99313e2ad52b4bb9b0924e2c
Ilias Baferos <ilias.baferos@sonova.com>
2022-10-06 20:31:32 +0200

    Rendering ready

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{error::Error, io, time::Duration};
+use std::{error::Error, io, time::Duration, sync::mpsc, thread};
 use crossterm::{terminal::{self, EnterAlternateScreen}, ExecutableCommand, cursor::{Hide, Show}, event::{self, KeyCode}};
 use rusty_audio::Audio;
+use space_invaders::{frame, render};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -18,11 +19,29 @@ fn main() -> Result<(), Box<dyn Error>> {
     terminal::enable_raw_mode()?;
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
+
+    // Render loop in a seperate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame =  match render_rx.recv() {
+                Ok(x) => x,  
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame= curr_frame;
+        }
+        
+    });
+
     
-
-
     // Game loop
     'mainGameLoop: loop{
+        //Per frame init
+        let mut curr_frame = frame::new_frame();
         //check for input
         while event::poll(Duration::default())? {
             if let event::Event::Key(key) = event::read()? {
@@ -34,10 +53,14 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+        //Draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
-
     //Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(terminal::LeaveAlternateScreen)?;
